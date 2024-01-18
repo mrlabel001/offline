16abe871f39c50455e0ab65f3889047f7e65a3f0
fabian <farbean.makini@student.moringaschool.com>
2024-01-18 06:53:28 -0800

    sql

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Owner, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -22,9 +22,33 @@ def pet_by_id(id):
 
     response_body = f'''
         <h2>Information for <h1>{pet.name}</h1></h2>
-        <h2>Pet Species is {pet.species}</h2>
-        <h2>Pet Owner is {pet.owner.name}</h2>
+        <h2>Pet Species is <h1>{pet.species}</h1></h2>
+        <h2>Pet Owner is <h1>{pet.owner.name}</h1></h2>
     '''
+
+    response = make_response(response_body, 200)
+
+    return response
+
+@app.route('/owner/<int:id>')
+def owner_by_id(id):
+    owner = Owner.query.filter(Owner.id == id).first()
+
+    if not owner:
+        response_body = '<h1>404 owner not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'<h1>Information for {owner.name}</h1>'
+
+    pets = [pet for pet in owner.pets]
+
+    if not pets:
+        response_body += f'<h2>Has no pets at this time.</h2>'
+
+    else:
+        for pet in pets:
+            response_body += f'<h2>Has pet {pet.species} named {pet.name}.</h2>'
 
     response = make_response(response_body, 200)
 
