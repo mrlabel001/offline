ff7f808241cf92d26395b9614f7681db35bd9faa
fabian <farbean.makini@student.moringaschool.com>
2024-01-18 06:21:03 -0800

    routes

--- app.py
+++ app.py
@@ -14,11 +14,21 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return '<h1>Welcome to my app!</h1>'
+    return '<h1>Welcome to my pet store!</h1>'
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    response_body = f'''
+        <h2>Information for <h1>{pet.name}</h1></h2>
+        <h2>Pet Species is {pet.species}</h2>
+        <h2>Pet Owner is {pet.owner.name}</h2>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
