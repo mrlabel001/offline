5a8bbb5a7a41fc9604a0387f317cb2f2845f7083
fabian <farbean.makini@student.moringaschool.com>
2024-01-18 08:56:44 -0800

    crud

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -15,44 +15,37 @@ db.init_app(app)
 @app.route('/')
 def index():
     return '<h1>Welcome to my pet store!</h1>'
-
-@app.route('/pets/<int:id>')
-def pet_by_id(id):
-    pet = Pet.query.filter(Pet.id == id).first()
-
-    response_body = f'''
-        <h2>Information for <h1>{pet.name}</h1></h2>
-        <h2>Pet Species is <h1>{pet.species}</h1></h2>
-        <h2>Pet Owner is <h1>{pet.owner.name}</h1></h2>
-    '''
-
-    response = make_response(response_body, 200)
-
+@app.route('/pets')
+def pets():
+    pets = []
+    for pet in Pet.query.all():
+        pet_dict = {
+            "species": pet.species,
+            "name": pet.name,
+        }
+        pets.append(pet_dict)
+    response = make_response(jsonify(pets), 200)
+    response.headers["Content_Type"] = "application/json"
     return response
+
+@app.route('/owners')
+def owners():
+    owners = Owner.query.all()
+    return owners.to_dict()
 
 @app.route('/owner/<int:id>')
 def owner_by_id(id):
-    owner = Owner.query.filter(Owner.id == id).first()
+    owner = Owner.query.get(id)
+    return owner.to_dict()
 
-    if not owner:
-        response_body = '<h1>404 owner not found</h1>'
-        response = make_response(response_body, 404)
-        return response
+@app.route('/owners/<int:id>')
+def owners_id(id):
+    oners = Owner.query.filter_by(id=id).first()
+    return oners.to_dict()
+    
 
-    response_body = f'<h1>Information for {owner.name}</h1>'
 
-    pets = [pet for pet in owner.pets]
 
-    if not pets:
-        response_body += f'<h2>Has no pets at this time.</h2>'
-
-    else:
-        for pet in pets:
-            response_body += f'<h2>Has pet {pet.species} named {pet.name}.</h2>'
-
-    response = make_response(response_body, 200)
-
-    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Owner(db.Model):
+class Owner(db.Model, SerializerMixin):
     __tablename__ = 'owners'
+
+    serialize_rules = ('-pets.owner',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
@@ -13,8 +16,10 @@ class Owner(db.Model):
     def __repr__(self):
         return f'<Pet Owner {self.name}>'
 
-class Pet(db.Model):
+class Pet(db.Model, SerializerMixin):
     __tablename__ = 'pets'
+
+    serialize_rules = ('-owners.pet',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
